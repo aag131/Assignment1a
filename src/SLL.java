fffbe65941489df6519d731b64b0d807e846af25
Alyssa Goodwin <aag131@uw.edu>
2023-10-21 19:55:36 -0700

    Add skeleton of requirements

--- src/SLL.java
+++ src/SLL.java
@@ -1,13 +1,41 @@
-public class SLL<Type> {
 /*
 Alyssa Goodwin
 CSS 342
 Assignment 1a
 Due: Oct 22, 2023
  */
-    private class Node {
-        Type elem;
-        Node next;
+public class SLL<Type> {
+    private int count;
+    public SLL() {
+        count = 0;
+    }
 
+    void addFront(Type elem) {
+
+    }
+
+    void addRear(Type elem) {
+
+    }
+
+    void add(int index, Type elem) {
+
+    }
+
+    Type delete(int index) {
+        return null;
+    }
+
+    Type get(int index) {
+        return null;
+    }
+
+    void swap(int index1, int index2) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
