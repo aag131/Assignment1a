1e54bbc139b742d023422bbc7328a21d14aec0c5
Alyssa Goodwin <aag131@uw.edu>
2023-10-21 19:35:58 -0700

    fsd

--- src/SLL.java
+++ src/SLL.java
@@ -1,3 +1,13 @@
-public class SLL {
+public class SLL<Type> {
+/*
+Alyssa Goodwin
+CSS 342
+Assignment 1a
+Due: Oct 22, 2023
+ */
+    private class Node {
+        Type elem;
+        Node next;
 
+    }
 }
